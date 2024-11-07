54adb166f06ff4a8e6f24bfdb572be1d4819cef7
koungq <guu119@naver.com>
2024-11-07 16:58:42 +0900

    feat: retrieve feed

--- src/main/java/org/example/domain/post/dto/PostResDTO.java
+++ src/main/java/org/example/domain/post/dto/PostResDTO.java
@@ -8,18 +8,18 @@ import java.time.LocalDateTime;
 
 public class PostResDTO {
 
-    public record PostSummary(
+    public record Detail(
             Long id,
             String content,
             Long likeCount,
             Long commentCount,
             String createdAt,
-
             String profileImg,
             String writer,
-            Boolean isVerified
+            Boolean isVerified,
+            Boolean isPinned
     ) {
-        public static PostSummary toPostSummary(ResultSet resultSet) throws SQLException {
+        public static Detail toDetail(ResultSet resultSet) throws SQLException {
             Long id = resultSet.getLong("id");
             String content = resultSet.getString("content");
             Long likeCount = resultSet.getLong("like_count");
@@ -28,8 +28,9 @@ public class PostResDTO {
             String writer = resultSet.getString("name");
             String profileImg = resultSet.getString("profile_image_url");
             Boolean isVerified = resultSet.getBoolean("is_verified");
+            Boolean isPinned = resultSet.getBoolean("is_pinned");
 
-            return new PostSummary(id, content, likeCount, commentCount, createdAt, profileImg, writer, isVerified);
+            return new Detail(id, content, likeCount, commentCount, createdAt, profileImg, writer, isVerified, isPinned);
         }
 
         private static String convertToString(Timestamp time) {
