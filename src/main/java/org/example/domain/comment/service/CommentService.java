44490251acdea8c1df7f193ed32e0cd705c4156a
koungq <guu119@naver.com>
2024-11-17 19:49:28 +0900

    Refactor: comment domain service architecture layer

--- src/main/java/org/example/domain/comment/service/CommentService.java
+++ src/main/java/org/example/domain/comment/service/CommentService.java
@@ -3,45 +3,38 @@ package org.example.domain.comment.service;
 import org.example.domain.comment.dto.CommentReqDTO.Save;
 import org.example.domain.comment.dto.CommentReqDTO.Update;
 import org.example.domain.comment.repository.CommentRepository;
-import org.example.domain.notice.service.NoticeService;
-import org.example.domain.post.service.PostService;
-import org.example.domain.user.service.UserService;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 import static org.example.domain.comment.dto.CommentResDTO.Detail;
-import static org.example.domain.notice.enums.NoticeMessage.COMMENT_ON_COMMENT;
-import static org.example.domain.notice.enums.NoticeMessage.COMMENT_ON_POST;
 
 public class CommentService {
 
     private final CommentRepository commentRepository = new CommentRepository();
-    private final PostService postService = new PostService();
-    private final NoticeService noticeService = new NoticeService();
-    private final UserService userService = new UserService();
 
-    public void save(Long userId, Save dto) {
-        commentRepository.save(userId, dto);
+    public void save(Long userId, Save dto, Connection connection) throws SQLException {
+        commentRepository.save(userId, dto, connection);
     }
 
-    public Long findWriter(Long commentId) {
-        return commentRepository.findWriter(commentId);
+    public Long findWriter(Long commentId, Connection connection) throws SQLException {
+        return commentRepository.findWriter(commentId, connection);
     }
 
-    public List<Detail> read(Long postId) {
-        return commentRepository.findComments(postId);
+    public List<Detail> read(Long postId, Connection connection) throws SQLException {
+        return commentRepository.findComments(postId, connection);
     }
 
-    public void update(Long commentId, Update dto) {
-        commentRepository.updateComment(commentId, dto);
+    public void update(Long commentId, Update dto, Connection connection) throws SQLException {
+        commentRepository.updateComment(commentId, dto, connection);
     }
 
-    public void delete(Long commentId, Long postId) {
-        Long deletedComments = commentRepository.delete(commentId);
-        postService.updateCommentCount(postId, -1 * deletedComments);
+    public Long delete(Long commentId, Connection connection) throws SQLException {
+        return commentRepository.delete(commentId, connection);
     }
 
-    public void updateLikeCount(Long commentId, Long value) {
-        commentRepository.updateLikeCount(commentId, value);
+    public void updateLikeCount(Long commentId, Long value, Connection connection) throws SQLException {
+        commentRepository.updateLikeCount(commentId, value, connection);
     }
 }
--- src/main/java/org/example/domain/comment/usecase/CommentUseCase.java
+++ src/main/java/org/example/domain/comment/usecase/CommentUseCase.java
@@ -1,11 +1,15 @@
 package org.example.domain.comment.usecase;
 
-import org.example.domain.comment.dto.CommentReqDTO;
 import org.example.domain.comment.dto.CommentReqDTO.Save;
+import org.example.domain.comment.dto.CommentReqDTO.Update;
+import org.example.domain.comment.dto.CommentResDTO.Detail;
 import org.example.domain.comment.service.CommentService;
 import org.example.domain.notice.service.NoticeService;
 import org.example.domain.post.service.PostService;
 import org.example.domain.user.service.UserService;
+import org.example.util.TransactionManager;
+
+import java.util.List;
 
 import static org.example.domain.notice.enums.NoticeMessage.COMMENT_ON_COMMENT;
 import static org.example.domain.notice.enums.NoticeMessage.COMMENT_ON_POST;
@@ -16,17 +20,39 @@ public class CommentUseCase {
     private final NoticeService noticeService = new NoticeService();
     private final PostService postService = new PostService();
     private final UserService userService = new UserService();
+    private final TransactionManager transactionManager = new TransactionManager();
 
     public void comment(Long userId, Save dto) {
-        if(dto.parentCommentId() == null) {
-            noticeService.notice(postService.findWriter(dto.postId()), COMMENT_ON_POST.getMessage(userService.findName(userId)));
-        } else {
-            Long writer = commentService.findWriter(dto.parentCommentId());
-            if(!userId.equals(writer))
-                noticeService.notice(writer, COMMENT_ON_COMMENT.getMessage(userService.findName(userId)));
-        }
+        transactionManager.execute(connection -> {
+            if(dto.parentCommentId() == null) {
+                noticeService.notice(postService.findWriter(dto.postId()), COMMENT_ON_POST.getMessage(userService.findName(userId)));
+            } else {
+                Long writer = commentService.findWriter(dto.parentCommentId(), connection);
+                if(!userId.equals(writer))
+                    noticeService.notice(writer, COMMENT_ON_COMMENT.getMessage(userService.findName(userId)));
+            }
 
-        commentService.save(userId, dto);
-        postService.updateCommentCount(dto.postId(), 1L);
+            commentService.save(userId, dto, connection);
+            postService.updateCommentCount(dto.postId(), 1L);
+        });
     }
+
+     public void deleteComment(Long commentId, Long postId) {
+        transactionManager.execute(connection -> {
+            Long deletedComments = commentService.delete(commentId, connection);
+            postService.updateCommentCount(postId, -1 * deletedComments);
+        });
+     }
+
+     public void updateComment(Long commentId, Update dto) {
+        transactionManager.execute(connection -> {
+            commentService.update(commentId, dto, connection);
+        });
+     }
+
+     public List<Detail> readComments(Long postId) {
+        return transactionManager.executeReadOnly(connection ->
+            commentService.read(postId, connection)
+        );
+     }
 }
