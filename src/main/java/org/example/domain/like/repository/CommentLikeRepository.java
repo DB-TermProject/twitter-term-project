096c5dac1b8029efce764d30da752b651c97586c
koungq <guu119@naver.com>
2024-11-17 20:03:47 +0900

    Refactor: like domain service architecture layer

--- src/main/java/org/example/domain/like/repository/CommentLikeRepository.java
+++ src/main/java/org/example/domain/like/repository/CommentLikeRepository.java
@@ -1,8 +1,5 @@
 package org.example.domain.like.repository;
 
-import org.example.util.config.JdbcConfig;
-import org.example.util.exception.SqlExecutionException;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -10,34 +7,24 @@ import java.sql.SQLException;
 public class CommentLikeRepository implements LikeRepository {
 
     @Override
-    public void save(Long userId, Long commentId) {
+    public void save(Long userId, Long commentId, Connection connection) throws SQLException {
         String sql = "INSERT INTO comment_like (liker_id, comment_id) values (?, ?)";
 
-        try (Connection connection = JdbcConfig.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)) {
-
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setLong(1, userId);
             statement.setLong(2, commentId);
-
             statement.executeUpdate();
-        } catch (SQLException e) {
-            throw new SqlExecutionException();
         }
     }
 
     @Override
-    public void delete(Long userId, Long commentId) {
+    public void delete(Long userId, Long commentId, Connection connection) throws SQLException {
         String sql = "DELETE FROM comment_like WHERE liker_id = ? AND comment_id = ?";
 
-        try (Connection connection = JdbcConfig.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)) {
-
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setLong(1, userId);
             statement.setLong(2, commentId);
-
             statement.executeUpdate();
-        } catch (SQLException e) {
-            throw new SqlExecutionException();
         }
     }
 }
